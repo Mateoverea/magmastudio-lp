aad4aa9fc96fba0c23461c2c1fcc88f9fb5a33c6
Mateo <mateoverea@hotmail.com>
2025-04-05 12:53:52 -0600

    Hero Improved

--- src/components/common/feature-cards.tsx
+++ src/components/common/feature-cards.tsx
@@ -50,31 +50,17 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
             >
               {descriptionMobile}
             </p>
-            {/*<div className=" mt-4 md:mt-8 relative inline-flex h-12 overflow-hidden rounded-full p-[3px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 group">
-              <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#67DBFF_0%,#393BB2_50%,#67DBFF_100%)]" />
-              <span
-                style={{
-                  backgroundColor: `${color}`,
-                }}
-                className={`text-${textColor} inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full px-2.5 md:px-6 py-1 text-base font-archivo font-medium backdrop-blur-3xl`}
-              >
-                Construye Tu Primer MVP
-                <ArrowRight className="ml-2 group-hover:-rotate-45 transition-all ease-in-out duration-500" />
-              </span>
-            </div>*/}
           </div>
 
           <div className="imageContainer mt-8 mb-2 md:mt-8">
             <motion.div className="inner">
-              {/*<video className=" scale-150 md:scale-100" autoPlay loop muted>
-                <source src={`/videos/${src}`} type="video/mp4" />
-              </video>*/}
               <Image
                 className="scale-100 md:scale-75"
                 src={`/lava/${src}`}
                 alt="Imagen animada"
                 width={650}
                 height={300}
+                style={{ width: "auto", height: "auto" }}
               />
             </motion.div>
           </div>
